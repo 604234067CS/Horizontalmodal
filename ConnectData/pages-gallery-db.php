79faf7f4f958222012dbc8baffd7720f5c4cf785
604234067CS <604234067@parichat.skru.ac.th>
2020-12-25 18:01:34 +0700

    modals

---
+++ ConnectData/pages-gallery-db.php
@@ -0,0 +1,60 @@
+<?php 
+
+require_once('pages-gallery-connectdb.php');
+
+        $target_dir = "../public/assets/images/gallery/";
+        $target_file = $target_dir.$_FILES['file_name']['name'];
+        $msg = '';
+
+        srand((double)microtime()*10000000);
+        $str = 'abcdefghijklmnopqrstuvwxyz0123456789'; //string ที่เป็นไปได้ที่จะใช้ในการ random ซึ่งสามารถเพิ่มลดได้ตามความต้องการ
+        $passw = str_shuffle($str);
+        $str_result = "";  //สตริงว่างสำหรับจะรับค่าจากการ random
+        $str_result .= substr($passw ,1,20); //ต่อ string จาก substring ที่ได้จากการ random ตำแหน่ง ทีละ 1 ตัว 
+                                //จนกว่าจะครบตรามความยาวที่ส่งมา
+
+                $file_name = explode(".", $_FILES['file_name']['name']);  
+                $allowed_ext = array("jpg", "jpeg", "png", "gif");  
+                    $new_name = $str_result . '.' . $file_name[1];  
+                    $sourcePath = $_FILES['file_name']['tmp_name'];  
+        //echo  $dir = $_GET['dir'];
+                    $target_file = '../public/assets/images/gallery/'.$new_name;  
+
+        if(move_uploaded_file($_FILES['file_name']['tmp_name'], $target_file)){
+        //$date_n = date('Y-m-d');
+        //$hour = date('H:i:s');
+        $sql = "INSERT INTO files (file_name, uploaded_on) VALUES ('".$new_name."', NOW())"; 
+        $insert = mysqli_query($conn, $sql);
+            //mysql_query($insert);
+        }else{
+            $msg = 'Error while uploading.';
+        }
+echo $msg;
+exit;
+// $msg = '';
+
+// srand((double)microtime()*10000000);
+// $str = 'abcdefghijklmnopqrstuvwxyz0123456789'; //string ที่เป็นไปได้ที่จะใช้ในการ random ซึ่งสามารถเพิ่มลดได้ตามความต้องการ
+// $passw = str_shuffle($str);
+// $str_result = "";  //สตริงว่างสำหรับจะรับค่าจากการ random
+// $str_result .= substr($passw ,1,20); //ต่อ string จาก substring ที่ได้จากการ random ตำแหน่ง ทีละ 1 ตัว 
+//                            //จนกว่าจะครบตรามความยาวที่ส่งมา
+//   echo  $count = count($_FILES['file']['name']);
+//     for($i=0;$i<$count;$i++){
+//       $file_name = explode(".", $_FILES['file']['name'][$i]);  
+//       $allowed_ext = array("jpg", "jpeg", "png", "gif");  
+//            $new_name = $str_result . '.' . $file_name[1];  
+//            $sourcePath = $_FILES['file']['tmp_name'][$i];  
+
+//            $target_file = 'uploads/'.$new_name;  
+//       if(move_uploaded_file($_FILES['file']['tmp_name'][$i], $target_file)){ 
+//       //$date_n = date('Y-m-d');
+//       //$hour = date('H:i:s');
+//       $sql = "INSERT INTO files (file_name, uploaded_on) VALUES ('".$new_name."', NOW())"; 
+//       $insert = mysqli_query($conn, $sql);
+//           // header('location: pages-galler.php'
+//           //mysql_query($insert);
+//       }
+//     }
+
+?>
---
+++ pages-gallery.php
@@ -0,0 +1,165 @@
+<?php include 'layouts/header.php'; ?>
+
+<!--Magnific Popup CSS -->
+<link rel="stylesheet" href="public/assets/plugins/magnific-popup/magnific-popup.css">
+
+<link href="public/assets/plugins/dropzone/dist/dropzone.css" rel="stylesheet" type="text/css">
+<?php include 'layouts/headerStyle.php'; ?>
+
+<body>
+
+    <?php include 'layouts/loader.php'; ?>
+
+    <?php include 'layouts/navbar.php'; ?>
+
+    <div class="wrapper">
+        <div class="container-fluid">
+
+            <!-- Page-Title -->
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="page-title-box">
+                        <div class="btn-group pull-right">
+                            <ol class="breadcrumb hide-phone p-0 m-0">
+                                <li class="breadcrumb-item"><a href="#">Forms</a></li>
+                                <li class="breadcrumb-item active">File Uploads</li>
+                            </ol>
+                        </div>
+                        <h4 class="page-title">File Uploads</h4>
+                    </div>
+                </div>
+            </div>
+            <!-- end page title end breadcrumb -->
+
+            <div class="row">
+                <div class="col-md-4 m-t-30">
+                    <div class="text-center">
+                        <!-- Large modal -->
+                        <button type="button" class="btn btn-primary waves-effect waves-light" data-toggle="modal" data-target=".bs-example-modal-lg">Click Upload</button>
+                    </div>
+                    <!--  Modal content for the above example -->
+                    <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-lg">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title mt-0" id="myLargeModalLabel">Upload Image</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="card m-b-20">
+                                                <div class="card-body">
+                                                    <div class="m-b-30">
+                                                        <form action="ConnectData/pages-gallery-db.php" class="dropzone" id="frmTarget" method="post" enctype="multipart/form-data">
+                                                            <div class="fallback">
+                                                                <input name="file_name" id="file_name" type="file" multiple>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                    <div class="text-center m-t-15">
+                                                        <input type="submit" id="startUpload" name="button" class="btn btn-primary waves-effect waves-light" value="Upload">
+                                                        <input type="button" class="btn btn-primary waves-effect waves-light" data-dismiss="modal" value="CLOSE" onClick="javascript:location.reload();">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div> <!-- end col -->
+                                    </div> <!-- end row -->
+                                </div>
+                            </div><!-- /.modal-content -->
+                        </div><!-- /.modal-dialog -->
+                    </div><!-- /.modal -->
+                </div>
+            </div>
+            <!-- Page-Title -->
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="page-title-box">
+                        <div class="btn-group pull-right">
+                            <ol class="breadcrumb hide-phone p-0 m-0">
+                                <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                                <li class="breadcrumb-item active">Gallery</li>
+                            </ol>
+                        </div>
+                        <h4 class="page-title">Gallery</h4>
+                    </div>
+                </div>
+            </div>
+            <!-- end page title end breadcrumb -->
+
+
+            <div class="ligthbox">
+                <?php
+                // Include the database configuration file 
+                require 'ConnectData/pages-gallery-connectdb.php';
+                // Get files from the database 
+                $query = "SELECT * FROM files ORDER BY id DESC";
+                $insert = mysqli_query($conn, $query);
+                if ($insert->num_rows > 0) {
+                    while ($row = $insert->fetch_assoc()) {
+                        $filePath = 'public/assets/images/gallery/' . $row["file_name"];
+                        $fileMime = mime_content_type($filePath);
+                ?>
+                        <embed src="<?php echo $filePath; ?>" type="<?php echo $fileMime; ?>" width="350px" height="240px" />
+                    <?php }
+                } else { ?>
+                    <p>No file(s) found...</p>
+                <?php } ?>
+            </div>
+        </div> <!-- end container -->
+    </div>
+    <!-- end wrapper -->
+
+    <?php include 'layouts/footer.php'; ?>
+
+    <?php include 'layouts/footerScript.php'; ?>
+
+    <!-- Magnific Popup -->
+    <script src="public/assets/plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+
+    <!-- Dropzone js -->
+    <script src="public/assets/plugins/dropzone/dist/dropzone.js"></script>
+
+    <!-- App js -->
+    <script src="public/assets/js/app.js"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <script>
+        Dropzone.autoDiscover = false;
+        $(function() {
+            //Dropzone class
+            var myDropzone = new Dropzone(".dropzone", {
+                url: "ConnectData/pages-gallery-db.php",
+                paramName: "file_name",
+                maxFilesize: 2,
+                parallelUploads: 15,
+                acceptedFiles: ".png,.jpg,.gif,.bmp,.jpeg",
+                autoProcessQueue: false
+            });
+
+            $('#startUpload').click(function() {
+                myDropzone.processQueue();
+            });
+        });
+    </script>
+
+    <script type="text/javascript">
+        $('.gallery-popup').magnificPopup({
+            type: 'image',
+            closeOnContentClick: true,
+            mainClass: 'mfp-fade',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+            }
+        });
+    </script>
+
+
+
+</body>
+
+</html>
